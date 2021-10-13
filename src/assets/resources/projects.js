ed541833ea857163ba9972af454105856fce475c
Meghan Bock <meghan.bock@gmail.com>
2021-10-13 02:42:27 -0500

    set up resume component and project list object

---
+++ src/assets/resources/projects.js
@@ -0,0 +1,64 @@
+const projects = [
+    {
+        id: 1,
+        title: "",
+        description: "",
+        date: "",
+        langauges: "",
+        image: "",
+        githubUrl: "",
+        deployedUrl: ""
+    },
+    {
+        id: 2,
+        title: "",
+        description: "",
+        date: "",
+        langauges: "",
+        image: "",
+        githubUrl: "",
+        deployedUrl: ""
+    },
+    {
+        id: 3,
+        title: "",
+        description: "",
+        date: "",
+        langauges: "",
+        image: "",
+        githubUrl: "",
+        deployedUrl: ""
+    },
+    {
+        id: 4,
+        title: "",
+        description: "",
+        date: "",
+        langauges: "",
+        image: "",
+        githubUrl: "",
+        deployedUrl: ""
+    },
+    {
+        id: 5,
+        title: "",
+        description: "",
+        date: "",
+        langauges: "",
+        image: "",
+        githubUrl: "",
+        deployedUrl: ""
+    },
+    {
+        id: 6,
+        title: "",
+        description: "",
+        date: "",
+        langauges: "",
+        image: "",
+        githubUrl: "",
+        deployedUrl: ""
+    }
+];
+
+export { projects };

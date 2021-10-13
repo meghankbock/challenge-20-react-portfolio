f8f6c2d2d94f66fadfb3a9485a5e34565cafe1b6
Meghan Bock <meghan.bock@gmail.com>
2021-10-13 01:04:50 -0500

    set up footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+        <section className="footer-container">
+            <ul>
+                <li>
+                    <span>Linkedin</span>
+                </li>
+                <li>
+                    <span>GitHub</span>
+                </li>
+            </ul>
+            <div className="container">&copy;2020 by Meghan Bock</div>
+        </section>
+    </footer>
+  );
+};
+
+export default Footer;

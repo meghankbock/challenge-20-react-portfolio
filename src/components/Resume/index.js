ae195a2921fee5221d0192944cecf747a5f20b08
Meghan Bock <meghan.bock@gmail.com>
2021-10-14 12:16:01 -0500

    additional tweaks to resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,14 +1,13 @@
-// import resume from "../../assets/resources/MeghanBockResume.pdf";
-
 const Resume = () => {
   function iframeLoaded() {
-    var iFrameID = document.getElementById("idIframe");
-    if (iFrameID) {
-      iFrameID.height = "";
-      iFrameID.height =
-        iFrameID.contentWindow.document.body.scrollHeight + "px";
+    let iframe = document.getElementById('iframeId');
+    console.log("this: ", iframe);
+    console.log("resize");
+    if (iframe) {
+      iframe.height =
+      iframe.contentWindow.document.body.offsetHeight + "px";
     }
-  }
+  };
 
   return (
     
@@ -17,11 +16,7 @@ const Resume = () => {
     <section id="resume-container" className="resume-container">
       <h2 className="primary-section-title">// Resume //</h2>
       <div className="resume-details flex-row">
-      <div id="resume-iframe"></div>
-      <script type="text/javascript" src="https://pym.nprapps.org/pym.v1.min.js"></script>
-        <script>
-            var pymParent = new pym.Parent({'resume-container', 'https://docs.google.com/document/d/e/2PACX-1vRxeuHmtsMl9ZqCxP35NUpdfO8Qbx8nBI8PinB3PHXmdhPyV7xPrtESOtM579kpD4wsz_2naBxm_OeH/pub?embedded=true'}, {});
-        </script>
+      <iframe id="iframeId" src="https://docs.google.com/document/d/e/2PACX-1vRxeuHmtsMl9ZqCxP35NUpdfO8Qbx8nBI8PinB3PHXmdhPyV7xPrtESOtM579kpD4wsz_2naBxm_OeH/pub?embedded=true"></iframe>
       </div>
     </section>
   );

f9a068865689fbe73ef769f68b0f30b11c22a2fc
Meghan Bock <meghan.bock@gmail.com>
2021-10-14 13:24:30 -0500

    update resume placement

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -16,7 +16,7 @@ const Resume = () => {
     <section id="resume-container" className="resume-container">
       <h2 className="primary-section-title">// Resume //</h2>
       <div className="resume-details flex-row">
-      <iframe id="iframeId" src="https://docs.google.com/document/d/e/2PACX-1vRxeuHmtsMl9ZqCxP35NUpdfO8Qbx8nBI8PinB3PHXmdhPyV7xPrtESOtM579kpD4wsz_2naBxm_OeH/pub?embedded=true"></iframe>
+      <iframe id="iframeId" frameborder='0' src="https://docs.google.com/document/d/e/2PACX-1vRxeuHmtsMl9ZqCxP35NUpdfO8Qbx8nBI8PinB3PHXmdhPyV7xPrtESOtM579kpD4wsz_2naBxm_OeH/pub?embedded=true"></iframe>
       </div>
     </section>
   );

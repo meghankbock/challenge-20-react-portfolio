34e82745ab57eabc0cf450aeac652ba41b30b173
Meghan Bock <meghan.bock@gmail.com>
2021-10-14 05:28:12 -0500

    format resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,16 +1,30 @@
 // import resume from "../../assets/resources/MeghanBockResume.pdf";
 
 const Resume = () => {
-  return (
-    <section className="resume-container">
-      <span className="sub-heading">
-        <h2>Resume</h2>
-      </span>
-      <div className="resume-details">
+  function iframeLoaded() {
+    var iFrameID = document.getElementById("idIframe");
+    if (iFrameID) {
+      iFrameID.height = "";
+      iFrameID.height =
+        iFrameID.contentWindow.document.body.scrollHeight + "px";
+    }
+  }
 
+  return (
+    
+
+
+    <section id="resume-container" className="resume-container">
+      <h2 className="primary-section-title">// Resume //</h2>
+      <div className="resume-details flex-row">
+      <div id="resume-iframe"></div>
+      <script type="text/javascript" src="https://pym.nprapps.org/pym.v1.min.js"></script>
+        <script>
+            var pymParent = new pym.Parent({'resume-container', 'https://docs.google.com/document/d/e/2PACX-1vRxeuHmtsMl9ZqCxP35NUpdfO8Qbx8nBI8PinB3PHXmdhPyV7xPrtESOtM579kpD4wsz_2naBxm_OeH/pub?embedded=true'}, {});
+        </script>
       </div>
     </section>
   );
-}
+};
 
 export default Resume;

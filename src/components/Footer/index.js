11cad17a921a60d9742a59e7ff859c8d45ce0867
Meghan Bock <meghan.bock@gmail.com>
2021-10-14 16:45:24 -0500

    additional css fixes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,29 +1,31 @@
-import { 
+import {
   FaCopyright,
   FaGithub,
   FaLinkedin,
   FaRegCopyright,
-  MdEmail
+  MdEmail,
 } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer>
-            <ul>
-                <li>
-                    <a href="https://www.linkedin.com/in/meghankbock/" target="_blank">
-                      <FaLinkedin size={30}/>
-                    </a>
-                </li>
-                <li>
-                    <a href="https://github.com/meghankbock" target="_blank">
-                      <FaGithub size={30}/>
-                    </a>
-                </li>
-                <li>
-                <h2>Made by Meghan Bock &copy; 2021.</h2>
-                </li>
-            </ul>
+      <section className="footer-container">
+        <ul>
+          <li>
+            <a href="https://www.linkedin.com/in/meghankbock/" target="_blank">
+              <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/meghankbock" target="_blank">
+              <FaGithub size={30} />
+            </a>
+          </li>
+          <li>
+            <h2>Made by Meghan Bock &copy; 2021.</h2>
+          </li>
+        </ul>
+      </section>
     </footer>
   );
 };
